8c6524a285294f6443a2d2f0f02c9933ed7b5d95
the-real-adammork <adammork@gmail.com>
2021-04-24 04:58:04 -0700

    add keyboard shortcut, quick readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Overview
+
+Chrome extension to speed up downloading music in the currently active tab. Needs [yt-server](https://github.com/the-real-adammork/yt-server) running on `localhost:3000`. 
+
+Then follow these directions to install the extension (skip to step 1) - https://developer.chrome.com/docs/extensions/mv3/getstarted/
+
+# Features
+
+- Click extension icon to download the current page. (Must be pinned to your toolbar)
+- OR, Use keyboard shortcut "CMD + SHIFT + Y" to download the current page.
+
+Puts final file in `~/Downloads`.
--- background.js
+++ background.js
@@ -1,26 +1,26 @@
 console.log("background.js")
 
-chrome.browserAction.onClicked.addListener(function(tab) { 
-  async function postData(url = '') {
-    // Default options are marked with *
-    const response = await fetch("http://localhost:3000/", {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      //credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'http://localhost:3000'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify({"link":url+""}) // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
-  }
+async function postData(url = '') {
+  // Default options are marked with *
+  const response = await fetch("http://localhost:3000/", {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    //credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'http://localhost:3000'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify({"link":url+""}) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
 
-  postData(tab.url)
+async function downloadURL(url = '') {
+  return postData(url)
     .then(function(response) {                      // first then()
         if(response.status == "success")
         {
@@ -36,5 +36,15 @@ chrome.browserAction.onClicked.addListener(function(tab) {
       console.log('Request failed', error);
       alert("yt-server error " + error)
     });
+}
 
+chrome.browserAction.onClicked.addListener(function(tab) { 
+  downloadURL(tab.url);
+});
+
+chrome.commands.onCommand.addListener(function(command) {
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
+      var url = tabs[0].url;
+      downloadURL(url);
+  });
 });
